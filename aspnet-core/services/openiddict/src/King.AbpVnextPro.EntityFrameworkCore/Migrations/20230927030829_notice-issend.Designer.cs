4b1a8a54556072c7dfa1144ce7a026d55b53db03
lm <1145603937@qq.com>
2023-09-27 11:09:26 +0800

    添加迁移信息

--- aspnet-core/services/openiddict/src/King.AbpVnextPro.EntityFrameworkCore/Migrations/20230927030829_notice-issend.Designer.cs
+++ aspnet-core/services/openiddict/src/King.AbpVnextPro.EntityFrameworkCore/Migrations/20230927030829_notice-issend.Designer.cs
@@ -4,6 +4,7 @@ using King.AbpVnextPro.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,10 @@ using Volo.Abp.EntityFrameworkCore;
 namespace King.AbpVnextPro.Migrations
 {
     [DbContext(typeof(AbpVnextProDbContext))]
-    partial class AbpVnextProDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230927030829_notice-issend")]
+    partial class noticeissend
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
