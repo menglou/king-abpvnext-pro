a899e57e6c606ce36ea83afda7aa8c2512ae2ae1
lm <1145603937@qq.com>
2023-09-27 08:43:16 +0800

    修复前端已知bug

--- templates/king-abp-vnext-pro-nuget-identityserver4-all/vue-element-admin/src/layout/components/Navbar.vue
+++ templates/king-abp-vnext-pro-nuget-identityserver4-all/vue-element-admin/src/layout/components/Navbar.vue
@@ -8,11 +8,11 @@
     />
 
     <breadcrumb
+      v-if="!topNav"
       id="breadcrumb-container"
       class="breadcrumb-container"
-      v-if="!topNav"
     />
-    <top-nav id="topmenu-container" class="topmenu-container" v-if="topNav" />
+    <top-nav v-if="topNav" id="topmenu-container" class="topmenu-container" />
     <div class="right-menu">
       <template v-if="device !== 'mobile'">
         <search id="header-search" class="right-menu-item" />
@@ -44,24 +44,24 @@
       </template>
 
       <!-- 通知 -->
-      <div class="right-menu-item hover-effect" v-if="device !== 'mobile'">
+      <div v-if="device !== 'mobile'" class="right-menu-item hover-effect">
         <el-popover
+          v-model="isShowUserNewsPopover"
           placement="bottom"
           trigger="click"
-          v-model="isShowUserNewsPopover"
           width="350"
         >
           <el-badge
-            @click.stop="showSettingPanel = !showSettingPanel"
-            :is-dot="true"
             slot="reference"
+            :is-dot="true"
+            @click.stop="showSettingPanel = !showSettingPanel"
           >
-            <i class="el-icon-bell" title="通知"></i>
+            <i class="el-icon-bell" title="通知" />
           </el-badge>
           <Notice
+            v-show="isShowUserNewsPopover"
             :messagelist="messagelist"
             :notificationlist="notificationlist"
-            v-show="isShowUserNewsPopover"
           />
         </el-popover>
       </div>
@@ -71,7 +71,7 @@
         trigger="click"
       >
         <div class="avatar-wrapper">
-          <img :src="avatar" class="user-avatar" />
+          <img :src="avatar" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
@@ -97,21 +97,21 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import { mapState } from "vuex";
-import Breadcrumb from "@/components/Breadcrumb";
-import TopNav from "@/components/TopNav";
-import Hamburger from "@/components/Hamburger";
-import ErrorLog from "@/components/ErrorLog";
-import Screenfull from "@/components/Screenfull";
-import SizeSelect from "@/components/SizeSelect";
-import LangSelect from "@/components/LangSelect";
-import Search from "@/components/HeaderSearch";
-import RightPanel from "@/components/RightPanel";
-import Notice from "@/components/Notice";
-import Settings from "./Settings";
-import { getFilePathByName } from "@/utils/abp";
-import { getBroadCastnoticelist, getcommonnoticelist } from "@/api/notcie";
+import { mapGetters } from 'vuex'
+import { mapState } from 'vuex'
+import Breadcrumb from '@/components/Breadcrumb'
+import TopNav from '@/components/TopNav'
+import Hamburger from '@/components/Hamburger'
+import ErrorLog from '@/components/ErrorLog'
+import Screenfull from '@/components/Screenfull'
+import SizeSelect from '@/components/SizeSelect'
+import LangSelect from '@/components/LangSelect'
+import Search from '@/components/HeaderSearch'
+import RightPanel from '@/components/RightPanel'
+import Notice from '@/components/Notice'
+import Settings from './Settings'
+import { getFilePathByName } from '@/utils/abp'
+import { getBroadCastnoticelist, getcommonnoticelist } from '@/api/notcie'
 export default {
   components: {
     Breadcrumb,
@@ -124,57 +124,57 @@ export default {
     Search,
     RightPanel,
     Settings,
-    Notice,
+    Notice
   },
   computed: {
-    ...mapGetters(["sidebar", "avatar", "device"]),
+    ...mapGetters(['sidebar', 'avatar', 'device']),
     ...mapState({
       showSettings: (state) => state.settings.showSettings,
       topNav() {
-        return this.$store.state.settings.topNav;
-      },
-    }),
+        return this.$store.state.settings.topNav
+      }
+    })
   },
   data() {
     return {
       showSettingPanel: false,
       isShowUserNewsPopover: false,
       messagelist: [],
-      notificationlist: [],
-    };
+      notificationlist: []
+    }
   },
   watch: {
     isShowUserNewsPopover(newval, oldval) {
       if (newval) {
-        getBroadCastnoticelist({ Status: 0,IsSend:true, Title: "" }).then((res) => {
-          this.notificationlist = [...res];
-        });
-        getcommonnoticelist({ Status: 0, IsSend:true,Title: "" }).then((res) => {
-          this.messagelist = [...res];
-        });
+        getBroadCastnoticelist({ Status: 0, IsSend: true, Title: '' }).then((res) => {
+          this.notificationlist = [...res]
+        })
+        getcommonnoticelist({ Status: 0, IsSend: true, Title: '' }).then((res) => {
+          this.messagelist = [...res]
+        })
       }
-    },
+    }
   },
   methods: {
     getFilePathByName,
     toggleSideBar() {
-      this.$store.dispatch("app/toggleSideBar");
+      this.$store.dispatch('app/toggleSideBar')
     },
     async logout() {
-      this.$confirm("确定注销并退出系统吗？", "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning",
-      }).then(async () => {
-        await this.$store.dispatch("user/logout");
-        this.$router.push(`/login?redirect=${this.$route.fullPath}`);
-      });
+      this.$confirm('确定注销并退出系统吗？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async() => {
+        await this.$store.dispatch('user/logout')
+        this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+      })
     },
     closesettingPanel() {
-      this.showSettingPanel = !this.showSettingPanel;
-    },
-  },
-};
+      this.showSettingPanel = !this.showSettingPanel
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
